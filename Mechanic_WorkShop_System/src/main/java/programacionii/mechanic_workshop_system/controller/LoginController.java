bde0640d039f6a2411081243774ffeb2bb5028f3
Gabriel Avilés <gabrielaviles240@gmail.com>
2021-06-16 07:58:27 -0600

    Estilos de interfaz

--- Mechanic_WorkShop_System/src/main/java/programacionii/mechanic_workshop_system/controller/LoginController.java
+++ Mechanic_WorkShop_System/src/main/java/programacionii/mechanic_workshop_system/controller/LoginController.java
@@ -7,23 +7,47 @@ package programacionii.mechanic_workshop_system.controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
  *
- * @author MateyR
+ * @author Sistemas-11
  */
-public class LoginController implements Initializable
-{
+public class LoginController implements Initializable {
+
+    @FXML
+    public TextField txtUser;
+    @FXML
+    public PasswordField txtPass;
+    @FXML
+    public Button btnLogging;
+    @FXML
+    public Hyperlink lblForgot;
+    @FXML
+    public Button btnClose;
 
     /**
      * Initializes the controller class.
      */
     @Override
-    public void initialize(URL url, ResourceBundle rb)
-    {
+    public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
-    
+    }
+
+    @FXML
+    public void btnCloseAction(ActionEvent event) {
+        Stage stage = (Stage) btnClose.getScene().getWindow();
+        stage.close();
+    }
 }
